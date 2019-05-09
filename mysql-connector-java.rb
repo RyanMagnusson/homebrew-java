f558a9dca10a7a3e6e89467ce91c67e1c7f93c0d
Ryan Magnusson <Ryan.Magnusson@walmart.com>
2019-05-09 13:47:37 -0500

    adding multiple versions of mysql java connector

--- mysql-connector-java.rb
+++ mysql-connector-java.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class MysqlConnectorJava < Formula
-  homepage "http://dev.mysql.com/downloads/connector/j/"
-  url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{version}.tar.gz"
   version "8.0.16"
   sha256 "de7d7f8ce9d777c2a6c7c47695d87bfd635e621f9142467a5e56538fcaf4644a"
+  homepage "http://dev.mysql.com/downloads/connector/j/"
+  url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{version}.tar.gz"
 
   def install
     prefix.install_metafiles
--- mysql-connector-java@5.1.42.rb
+++ mysql-connector-java@5.1.42.rb
@@ -2,10 +2,10 @@ require "formula"
 require_relative "mysql-connector-java"
 
 class MysqlConnectorJavaAT5142 < Formula
-  homepage "http://dev.mysql.com/downloads/connector/j/"
-  version "5.1.42"
   url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{version}.tar.gz"
   sha256 "4d261f669f685cd9e1eb5683d03019466cb9e59a8b873efce1b623254ea29231"
+  homepage "http://dev.mysql.com/downloads/connector/j/"
+  version "5.1.42"
 
   def install
     prefix.install_metafiles
